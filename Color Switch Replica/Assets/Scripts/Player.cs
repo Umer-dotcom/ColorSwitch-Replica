aa9ee954eb3f11b868a4b3c533248a9bf70e19fc
Muhammad Umer Nadeem <76922950+Umer-dotcom@users.noreply.github.com>
2021-09-11 11:43:43 +0500

    Basic Setup!

--- Color Switch Replica/Assets/Scripts/Player.cs
+++ Color Switch Replica/Assets/Scripts/Player.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement Properties")]
     [SerializeField] private float jumpForce = 10f;
 
+    [Header("Colour Properties")]
+    [SerializeField] private string currentColour;
+    [SerializeField] private Color[] colours;
+
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
     private void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
+
+        ChangeColour();
     }
 
     private void Update()
@@ -18,6 +27,44 @@ public class Player : MonoBehaviour
         if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void ChangeColour()
+    {
+        int index = Random.Range(0, colours.Length);
+
+        switch (index)
+        {
+            case 0:
+                currentColour = "Red";
+                sr.color = colours[0];
+                break;
+            case 1:
+                currentColour = "Blue";
+                sr.color = colours[1];
+                break;
+            case 2:
+                currentColour = "Yellow";
+                sr.color = colours[2];
+                break;
+            case 3:
+                currentColour = "Green";
+                sr.color = colours[3];
+                break;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(currentColour))
+        {
+            Debug.Log("I can pass");
+        }
+        else
+        {
+            Debug.Log("Game Over");
         }
     }
 
